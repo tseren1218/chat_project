c0ec0ca31f07bf4f09e5e8e01249252e57bf79b6
tseren1218 <tseren1218@gmail.com>
2023-12-05 18:21:09 +0800

    added port

--- node/script.js
+++ node/script.js
@@ -1,4 +1,4 @@
-const socket = io('https://tsedo-chat.vercel.app/');
+const socket = io('https://tsedo-chat.vercel.app:3000');
 const messageContainer = document.getElementById('message-container');
 const messageForm = document.getElementById('input-form')
 const messageInput = document.getElementById('input-field');
